afb800ccfea760a4a9dda3fd1cfa1f1240516985
Jan Kronquist <jan.kronquist@gmail.com>
2011-12-07 22:10:28 +0100

    Added JSON sample

--- src/main/java/com/jayway/rest/HerokuResource.java
+++ src/main/java/com/jayway/rest/HerokuResource.java
@@ -2,7 +2,9 @@ package com.jayway.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
@@ -13,4 +15,10 @@ public class HerokuResource {
 		return Response.ok("param was: " + msg).build();
 	}
 
+	@GET
+	@Path("/person")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response person(@QueryParam("first") String first, @QueryParam("last") String last) {
+		return Response.ok(new PersonDTO(first, last)).build();
+	}
 }
